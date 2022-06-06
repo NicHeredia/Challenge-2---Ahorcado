d3352e2bf2c5ea94627e95cdf586c96cab0eb64d
Nicolas Heredia <nicoherediasp@gmail.com>
2022-06-06 00:20:07 -0300

    Version casi final

---
+++ js/Ahorcado-Juego.js
@@ -0,0 +1,119 @@
+//arrays que contienen las palabras de la categoria elegida
+var equiposFutbol = ["BOCA JUNIORS", "BARCELONA", "ARSENAL", "MILAN","INDEPENDIENTE","RACING"];
+var animales = ["PERRO", "GATO", "CABALLO", "CABRA", "CERDO"];
+var tecnologia = ["IPHONE", "ANDROID", "NETFLIX", "AMAZON", "JAVASCRIPT"];
+
+//capturar que boton se presiono en pantallaCategorias
+var botonFutbol = document.getElementById("botonFutbol");
+var botonAnimales = document.querySelector("#botonAnimales");
+var botonTecnologia = document.querySelector("#botonTecnologia");
+var botonGuardarYJugar = document.querySelector("#botonGuardarYJugar");
+
+
+//Elegir palabra random y crear los guiones correspondientes con las letras
+//ocultas
+function crearGuionesPalabra(array){
+    var palabraSecreta = array[Math.floor(Math.random()*array.length)];
+    var tableroJuego = document.getElementById("tablero-juego");
+    
+    for(let i = 0; i <= (palabraSecreta.length - 1); i++){
+        var div = document.createElement("div");
+        var span = document.createElement("span");
+        div.classList.add("guionPalabraSecreta");
+        span.classList.add("letra-oculta");
+        div.appendChild(span);
+        span.textContent = palabraSecreta[i];
+        tableroJuego.appendChild(div);
+    }
+    return palabraSecreta;
+}
+
+//funcionamiento de los botones que inician el juego
+
+botonFutbol.addEventListener("click", function(e){
+    transicionPantallas(pantallaCategorias,"none",pantallaJuego,"block");
+    crearGuionesPalabra(equiposFutbol);
+})
+
+botonAnimales.addEventListener("click", function(e){
+    transicionPantallas(pantallaCategorias,"none",pantallaJuego,"block");
+    crearGuionesPalabra(animales);
+})
+
+botonTecnologia.addEventListener("click", function(e){
+    transicionPantallas(pantallaCategorias,"none",pantallaJuego,"block");
+    crearGuionesPalabra(tecnologia);
+})
+
+
+// Funcionamiento de la seccion de agregar una palabra
+
+var inputNuevaPalabra = document.querySelector("#input-nueva-palabra");
+
+inputNuevaPalabra.addEventListener("keyup", function(e){
+    inputNuevaPalabra.value = inputNuevaPalabra.value.toUpperCase();
+    inputNuevaPalabra.value = inputNuevaPalabra.value.replace(/[^A-Z]/, "");
+    
+})
+
+botonGuardarYJugar.addEventListener("click", function(e){
+    var palabraElegida = [];
+    palabraElegida.push(inputNuevaPalabra.value);
+    crearGuionesPalabra(palabraElegida);
+    transicionPantallas(pantallaAgregarPalabra,"none",pantallaJuego,"block");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//funcionamiento del canvas
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/TransicionesYBotones.js
@@ -0,0 +1,81 @@
+// Variables que seleccionan pantalla
+
+var pantallaPrincipal = document.querySelector("#pantalla-principal");
+var pantallaComoJugar = document.querySelector("#pantalla-comoJugar");
+var pantallaCategorias = document.querySelector("#pantalla-categorias");
+var pantallaAgregarPalabra = document.querySelector("#pantalla-agregarPalabra");
+var pantallaJuego = document.querySelector("#pantalla-juego");
+
+
+// variables que seleccionan botones correspondientes
+
+var botonNuevoJuego = document.getElementById("botonNuevoJuego");
+var botonComoJugar = document.querySelector("#botonComoJugar");
+var botonVolver = document.querySelector("#botonVolver");
+var botonJuegoPersonalizado = document.querySelector("#boton-juegoPersonalizado");
+var botonJugar = document.querySelector("#botonJugar");
+
+//funcion que realiza la transicion entre las pantallas
+function transicionPantallas(pantalla1, propiedad1, pantalla2, propiedad2){
+    pantalla1.style.display = propiedad1;
+    pantalla2.style.display = propiedad2;
+}
+
+//funcionamiento de los botones
+
+botonNuevoJuego.addEventListener("click", function(e){
+    transicionPantallas(pantallaPrincipal, "none", pantallaCategorias, "block");
+})
+
+botonJuegoPersonalizado.addEventListener("click", function(e){
+    transicionPantallas(pantallaCategorias, "none", pantallaAgregarPalabra, "flex");
+})
+
+botonVolver.addEventListener("click", function(e){
+    transicionPantallas(pantallaAgregarPalabra,"none",pantallaCategorias,"block");
+})
+
+botonComoJugar.addEventListener("click", function(e){
+    transicionPantallas(pantallaPrincipal, "none", pantallaComoJugar, "block");
+})
+
+botonJugar.addEventListener("click", function(e){
+    transicionPantallas(pantallaComoJugar, "none", pantallaCategorias,"block");
+})
+
+
+// Funcionamiento de la seccion de agregar una palabra
+
+// var palabraElegida = "";
+// var inputNuevaPalabra = document.querySelector("#input-nueva-palabra");
+
+// inputNuevaPalabra.addEventListener("keyup", function(e){
+//     inputNuevaPalabra.value = inputNuevaPalabra.value.toUpperCase();
+//     inputNuevaPalabra.value = inputNuevaPalabra.value.replace(/[^A-Z]/, "");
+    
+// })
+
+// botonGuardarYJugar.addEventListener("click", function(e){
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
